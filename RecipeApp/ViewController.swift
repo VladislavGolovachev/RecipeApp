df6f95ab4028a2caca4463117764b7627b4c98ac
VladislavGolovachev <serpentess14@gmail.com>
2024-07-23 19:19:03 +0500

    Initial project

---
+++ RecipeApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RecipeApp
+//
+//  Created by Владислав Головачев on 23.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var collectionView = UICollectionView()
+        collectionView.add
+    }
+}
+
